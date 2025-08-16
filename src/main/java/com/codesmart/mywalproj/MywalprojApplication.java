b025bd7c8da1e53218dbab995b64ceb15499d510
Gebre Reda <gebre15tsegay@gmail.com>
2025-08-15 23:25:21 -0700

    my wal app

---
+++ src/main/java/com/codesmart/mywalproj/MywalprojApplication.java
@@ -0,0 +1,13 @@
+package com.codesmart.mywalproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywalprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywalprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesmart/mywalproj/MywalprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesmart.mywalproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywalprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
